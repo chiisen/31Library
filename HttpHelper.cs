8201d641f933dea2669b3fca241932b18d927f4c
DESKTOP-HUDNBUM\sam <sam@super-royal.com>
2023-04-20 16:18:54 +0800

    feat: 可以 Post 指定的內容到指定的網址，並且可以取得回傳的內容。

---
+++ HttpHelper.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace _31Library
+{
+    /// <summary>
+    /// Http 小幫手
+    /// </summary>
+    public class HttpHelper
+    {
+        /// <summary>
+        /// 避免使用 HttpClient 產生太多連線
+        /// </summary>
+        private readonly IHttpClientFactory _httpClientFactory;
+        /// <summary>
+        /// 預設的 Timeout 時間，單位秒
+        /// </summary>
+        private readonly int _defaultTimeout = 14;
+
+        /// <summary>
+        ///  建構 HttpHelper 強制必須帶入 IHttpClientFactory
+        /// </summary>
+        public HttpHelper(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        /// <summary>
+        /// Post
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="postData"></param>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        public async Task<(string body, HttpStatusCode statusCode)> Post(string url, object? postData, Dictionary<string, string> headers = null)
+        {
+            using (var request = _httpClientFactory.CreateClient("log"))
+            {
+                if (headers != null)
+                {
+                    foreach (var item in headers)
+                    {
+                        request.DefaultRequestHeaders.Add(item.Key, item.Value);
+                    }
+                }
+
+                request.Timeout = TimeSpan.FromSeconds(_defaultTimeout);
+                var response = await request.PostAsync(url, new StringContent(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json"));
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return (body, response.StatusCode);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# 31Library
-31 Library
+# 31Library  
+31 Library  
+- HttpHelper  
+	Http 小幫手，可以 Post 指定的內容到指定的網址，並且可以取得回傳的內容。  
+	使用此物件可以避免使用 HttpClient 產生太多連線的問題。  
+	
